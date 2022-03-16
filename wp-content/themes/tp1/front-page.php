51ee495a86ccfb570d8a8af53f00d2e9a9f4b380
Sara Hébert <e1995310@cmaisonneuve.qc.ca>
2022-03-16 09:38:13 -0400

    Reset!

--- wp-content/themes/tp1/front-page.php
+++ wp-content/themes/tp1/front-page.php
@@ -1,15 +1,15 @@
 <?php get_header() ?>
 <main class="site__main">
-<h1>Bienvenue!</h1>
+<h1>Groupe #2</h1>
 <section class="animation">
-    <div class="animation__bloc">b</div>
-    <div class="animation__bloc">o</div>
-    <div class="animation__bloc">r</div>
-    <div class="animation__bloc">p</div>
-    <div class="animation__bloc">a</div>
+    <div class="animation__bloc">1</div>
+    <div class="animation__bloc">2</div>
+    <div class="animation__bloc">3</div>
+    <div class="animation__bloc">4</div>
+    <div class="animation__bloc">5</div>
 </section>
 
-    <h1>Intégration multimedia!</h1>
+    <h1>---- Front-page.php ------</h1>
    <?php if (have_posts()): the_post(); ?>
         <?php the_title() ?>
         <?php the_content() ?>   
