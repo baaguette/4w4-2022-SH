1da0ce35aa74b67905c01885c5ff4fcf691adb41
Sara Hébert <e1995310@cmaisonneuve.qc.ca>
2022-04-26 15:24:55 -0400

    hotfix + color changes

--- wp-content/themes/TRUE/category.php
+++ wp-content/themes/TRUE/category.php
@@ -9,31 +9,42 @@
 
 <?php $url_categorie_slug = trouve_la_categorie(array('cours','web','jeu','design', 'utilitaire', 'creation-3d', 'video')); ?>
 
+<section class="radiobutton">
+
+  <input type="radio" checked="checked" name="radio">582 – TIM - Technique d’intégration multimédia
+  <span class="checkmark"></span>
+  <input type="radio" name="radio">393 – TDD - Techniques de la documentation
+  <span class="checkmark" ></span>
+  <input type="radio" name="radio">350 -  PSY – Psychologie
+  <span class="checkmark"></span>
+
+</section>
 <?php 
  $ordre = get_query_var('ordre', 'asc');
 ?>
-<a href="?cletri=title&ordre=asc">Ascendant</a><br>
-<a href="?cletri=title&ordre=desc">Descendant</a><br>
-
 
 
         <h2 class="formation__titre">Liste des cours du programme TIM</h2>
+        <div class="assdess">
+        <a class="up" href="?cletri=title&ordre=asc">↥</a><br>
+        <a class="down" href="?cletri=title&ordre=desc">↧</a><br>
+        </div>
         <?php 
-        /*
+        
 
         if (is_category('web')){ 
             
             $ma_categorie = get_category_by_slug('web');
             echo "<h3>" . $ma_categorie->description . "</h3>";
         }
-        */
-        /*
+        
+        
         if (is_category('jeu')){ echo "<h3>Cours Jeu</h3>";}
         if (is_category('design')){ echo "<h3>Cours design</h3>";}
         if (is_category('utilitaire')){ echo "<h3>Cours utilitaire</h3>";}
         if (is_category('creation-3d')){ echo "<h3>Cours création 3D</h3>";}
         if (is_category('video')){ echo "<h3>Cours vidéo</h3>";}
-        */
+        
         // retourne un string qui représente le slug de la catégorie
         
         $ma_categorie = get_category_by_slug($url_categorie_slug);
--- wp-content/themes/TRUE/gabarits/content-cours.php
+++ wp-content/themes/TRUE/gabarits/content-cours.php
@@ -12,7 +12,7 @@ $categories = get_the_category();
     // $nbHeures = substr($titre, -6);
     $nbHeures = get_field( "nombre_dheures" );
     $departement = get_field( "departement" );
-    $sigleCours = substr($titre, 0, 4);
+    //$sigleCours = substr($titre, 0, 4);
     $descCours  = get_the_content(); // description complète du cours */
 
     ?>
@@ -25,6 +25,6 @@ $categories = get_the_category();
     </h3>
     <div class="cours__nbre-heure"><?= $nbHeures; ?></div>
     <p class="cours__sigle"><?= $sigleCours; ?> </p>
-    <p class="cours__desc"> <?= wp_trim_words($descCours,15,"<button class='cours__desc__ouvrir'> La suite </button>"); ?></p>
+    <p class="cours__desc"> <?= wp_trim_words($descCours,25,"<button class='cours__desc__ouvrir'> ---> </button>"); ?></p>
     <p class="cours__departement"><?= $departement; ?></p>
 </article>
