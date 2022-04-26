e9dbf5acc38df42e43effe9b8831d016d62c435a
Sara Hébert <e1995310@cmaisonneuve.qc.ca>
2022-04-25 20:27:28 -0400

    hotfixx

--- wp-content/themes/TRUE/functions.php
+++ wp-content/themes/TRUE/functions.php
@@ -1,41 +1,67 @@
 <?php 
 
-
 require_once("options/apparences.php");
 
+/* --------------------------------------------------------------------  add_theme_support() */
+function cidw_4w4_add_theme_support(){
+add_theme_support( 'post-thumbnails' );
+add_theme_support( 'custom-logo', array(
+    'height' => 100,
+    'width'  => 100,
+) );
+}
+
+add_action('after_setup_theme', 'cidw_4w4_add_theme_support');
+/* ------------------------------------------------------------------------------------------ */
 
 function cidw_4w4_enqueue(){
     //wp_enqueue_style('style_css', get_stylesheet_uri());
-    wp_enqueue_style('cidw-4w4-le-style', get_template_directory_uri() . '/style.css', array(), filemtime(get_template_directory() . '/style.css'), false);
-    wp_enqueue_style('cidw-4w4-police-google',"https://fonts.googleapis.com/css2?family=Montserrat:wght@500&family=Poppins:wght@300;400;500&family=Roboto&display=swap");
-    wp_enqueue_script('cidw-4w4-boite-modale', 
+    wp_enqueue_style('cidw-4w4-le-style',
+                        get_template_directory_uri() . '/style.css',
+                        array(), 
+                        filemtime(get_template_directory() . '/style.css'), 
+                        false);
+
+    wp_enqueue_script('cidw-4w4-boite-modale',
                         get_template_directory_uri() . '/javascript/boite-modale.js',
                         array(),
                         filemtime(get_template_directory() . '/javascript/boite-modale.js'),
-                        true);
-   
-   
+                        true); // true pour intégrer le js en bas du document
+
+    wp_enqueue_style('cidw-4w4-google-font',"https://fonts.googleapis.com/css2?family=Montserrat:wght@500&family=Poppins:wght@300;400;500&family=Roboto&display=swap", false);
     
 
 }
 
-
 add_action("wp_enqueue_scripts", "cidw_4w4_enqueue");
 
-/* -------------------------------------------------- Enregistré le menu */
+/* ---------------------------------------------------------------------------------------------- Enregistré le menu */
 function cidw_4w4_register_nav_menu(){
     register_nav_menus( array(
         'menu_principal' => __( 'Menu principal', 'cidw_4w4' ),
         'menu_footer'  => __( 'Menu footer', 'cidw_4w4' ),
-        'menu_lien_externe'  => __( 'Menu lien externe', 'cidw_4w4' ),
-        'menu_cours' => __('Menu categories cours', 'cidw_4w4'),
-        'menu_accueil' => __('Menu accueil', 'cidw_4w4'),
-        'menu_accueil_evenement' => __('Menu accueil événement', 'cidw_4w4'),
+        'footer_colonne'  => __( 'Menu footer colonne', 'cidw_4w4' ),
+        'menu_cours'  => __( 'Menu cours', 'cidw_4w4' ),
+        'menu_accueil' => __( 'Menu  accueil', 'cidw_4w4' ),
+
     ) );
 }
 add_action( 'after_setup_theme', 'cidw_4w4_register_nav_menu', 0 );
 
-/* ---------------------------------------------------------------------- filtré les choix du menu principal */
+/* ---------------------------------------------------- afficher une description de choix de menu */
+/* Cette nouvelle version permet de ne pas avoir de warning */
+function prefix_nav_description( $item_output, $item) {
+    if ( !empty( $item->description ) ) {
+        $item_output = str_replace( '</a>',
+        '<hr><span class="menu-item-description">' . $item->description . '</span><div class="menu-item-icone"></div></a>',
+              $item_output );
+    }
+    return $item_output;
+}
+add_filter( 'walker_nav_menu_start_el', 'prefix_nav_description', 10, 2 );
+
+
+/* ---------------------------------------------------------------------------------------------- filtré les choix du menu principal */
 function cidw_4w4_filtre_choix_menu($obj_menu){
     //var_dump($obj_menu);
     foreach($obj_menu as $cle => $value)
@@ -49,45 +75,30 @@ function cidw_4w4_filtre_choix_menu($obj_menu){
 }
 add_filter("wp_nav_menu_objects","cidw_4w4_filtre_choix_menu");
 
-/* ----------------------------------------------------------- Ajout de la description dans menu */
-function prefix_nav_description( $item_output, $item) {
-    if ( !empty( $item->description ) ) {
-        $item_output = str_replace( '</a>',
-        '<hr><span class="menu-item-description">' . $item->description . '</span><div class="menu-item-icone"></div></a>',
-              $item_output );
-    }
-    return $item_output;
-}
-add_filter( 'walker_nav_menu_start_el', 'prefix_nav_description', 10, 2 );
-// l'argument 10 : niveau de privilège
-// l'argument 2 : le nombre d'argument dans la fonction de rappel: «prefix_nav_description»
-
-/* -----------------------------------------------------------   add_theme_support() */
-function cidw_4w4_add_theme_support()
-{
-    add_theme_support('post-thumbnails');
-
-    add_theme_support( 'custom-logo', array(
-        "width" => 100,
-        "height" => 100
-    ));
-}
- 
-add_action( 'after_setup_theme', 'cidw_4w4_add_theme_support' );
 
 
-
-
-/*---------------------------------------------------------- Enregistrement des sidebar */
-
-
+/* -------------------------------------------------------------------- Enregistrement des sidebar */
+add_action( 'widgets_init', 'my_register_sidebars' );
 function my_register_sidebars() {
     /* Register the 'primary' sidebar. */
     register_sidebar(
         array(
-            'id'            => 'pied_page_colonne_1',
-            'name'          => __( 'Pied de page colonne 1' ),
-            'description'   => __( 'Colonne de pied de page' ),
+            'id'            => 'entete_1',
+            'name'          => __( 'Entete 1' ),
+            'description'   => __( 'Ce sidebar s\'affiche dans l\'entête du site' ),
+            'before_widget' => '<div id="%1$s" class="widget %2$s">',
+            'after_widget'  => '</div>',
+            'before_title'  => '<h3 class="widget-title">',
+            'after_title'   => '</h3>',
+        )
+    );
+
+
+    register_sidebar(
+        array(
+            'id'            => 'footer_colonne_1',
+            'name'          => __( 'Footer colonne 1' ),
+            'description'   => __( 'Ce sidebar s\'affiche dans une colonne du pied de page' ),
             'before_widget' => '<div id="%1$s" class="widget %2$s">',
             'after_widget'  => '</div>',
             'before_title'  => '<h3 class="widget-title">',
@@ -97,9 +108,9 @@ function my_register_sidebars() {
 
     register_sidebar(
         array(
-            'id'            => 'pied_page_colonne_2',
-            'name'          => __( 'Pied de page colonne 2' ),
-            'description'   => __( 'Colonne de pied de page' ),
+            'id'            => 'footer_colonne_2',
+            'name'          => __( 'Footer colonne 2' ),
+            'description'   => __( 'Ce sidebar s\'affiche dans une colonne du pied de page' ),
             'before_widget' => '<div id="%1$s" class="widget %2$s">',
             'after_widget'  => '</div>',
             'before_title'  => '<h3 class="widget-title">',
@@ -109,9 +120,9 @@ function my_register_sidebars() {
 
     register_sidebar(
         array(
-            'id'            => 'pied_page_colonne_3',
-            'name'          => __( 'Pied de page colonne 3' ),
-            'description'   => __( 'Colonne de pied de page' ),
+            'id'            => 'footer_colonne_3',
+            'name'          => __( 'Footer colonne 3' ),
+            'description'   => __( 'Ce sidebar s\'affiche dans une colonne du pied de page' ),
             'before_widget' => '<div id="%1$s" class="widget %2$s">',
             'after_widget'  => '</div>',
             'before_title'  => '<h3 class="widget-title">',
@@ -121,35 +132,31 @@ function my_register_sidebars() {
 
     register_sidebar(
         array(
-            'id'            => 'pied_page_ligne_1',
-            'name'          => __( 'Pied de page ligne 1' ),
-            'description'   => __( 'Colonne de pied de page' ),
+            'id'            => 'footer_ligne_1',
+            'name'          => __( 'Footer ligne 1' ),
+            'description'   => __( 'Ce sidebar s\'affiche dans une ligne du pied de page' ),
             'before_widget' => '<div id="%1$s" class="widget %2$s">',
             'after_widget'  => '</div>',
             'before_title'  => '<h3 class="widget-title">',
             'after_title'   => '</h3>',
         )
     );
+
     /* Repeat register_sidebar() code for additional sidebars. */
 }
 
-add_action( 'widgets_init', 'my_register_sidebars' );
-
-/**
- * Retourne le slug de la catégorie  contenu dans le $tableau si cette catégorie représente la catégorie contenu dans l'URL. 
- * 
- * @param array $tableau  qui contient l'ensemble des slug de catégorie que l'on veut valider
- * 
- * @return string le slug de la catégorie
- */
-function trouve_la_categorie($tableau){
+ /**
+  * Extraire le slug de la catégorie de l'url 
+  * @param array $tableau : liste des slug de catégorie de la page
+  * @return string $cle : le slug de la catégorie  
+  */
+  function trouve_la_categorie($tableau){
     foreach($tableau as $cle){
-        if(is_category($cle)) return($cle);
+        if (is_category($cle)) return($cle);
     }
-}
+  }
 
-
-/**
+  /**
  * Modifie la requête global de WP_query.
  * 
  * @param WP_query $query : Objet contenant la requête global.
@@ -165,27 +172,43 @@ function cidw_4w4_pre_get_posts(WP_Query $query)
     }        
     else
     {
-        $ordre = get_query_var('ordre', 'asc');
+        $ordre = get_query_var('ordre','asc');
         $cle = get_query_var('cletri','title');       
         $query->set('order',  $ordre);
         $query->set('orderby', $cle);
+        /*
+        $query->set('order',  'asc');
+        $query->set('orderby', 'title');
+        */
         $query->set('postperpage','-1');
         return $query;
     }
 }
 
 
+
 function cidw_4w4_query_vars($params){
+// var_dump($params);
+// die();
+    $params[] = "ordre";
+    $params[] = "cletri";
+return $params;
+
+}
+/*
     $params[] = "cletri";
     $params[] = "ordre";
+    //$params["cletri"] = "title";
+    //var_dump($params); die();
     return $params;
 }
+
+*/
 add_action('pre_get_posts', 'cidw_4w4_pre_get_posts');
-/* Le hook «pre_get_posts» nous permet d'alterer les composante de la requête WP_query */
+/* 
+Que le hook «pre_get_posts» se manisfeste juste avant que la requête WP_query soit exécuté. 
+Ce hook nous permettra d'adapter la requête avant d'exécuter cette requête 
+*/ 
+
 add_filter('query_vars', 'cidw_4w4_query_vars' );
-/* Le hook «query_vars» nous permet d'alterer les arguments de l'URL */
-
-
-
-
 ?>
--- wp-content/themes/TRUE/javascript/boite-modale.js
+++ wp-content/themes/TRUE/javascript/boite-modale.js
@@ -5,13 +5,18 @@
   let cours__desc__ouvrir = document.querySelectorAll(".cours__desc__ouvrir");
   let boite__modale__fermeture = document.querySelector(".boite__modale__fermeture");
   console.log(cours__desc__ouvrir.length);
-  for (const bout of cours__desc__ouvrir) {
-      // console.log(bout.tagName);
-      bout.addEventListener('mousedown', function(){
-          // console.log(this.parentNode.parentNode.className);
-          console.log(this.parentNode.parentNode.children[0].innerHTML);
-          boite__modale.classList.add('ouvrir');
-          boite__modale__texte.innerHTML = this.parentNode.parentNode.children[0].innerHTML;
+
+
+   for (const bout of cours__desc__ouvrir) {
+     console.log(bout.tagName)
+     bout.addEventListener('mousedown', function(){
+         console.log(this.parentNode.parentNode.children[0].innerHTML)
+     
+         boite__modale.classList.add('ouvrir')
+         console.log(boite__modale.classList)
+        boite__modale__texte.innerHTML = this.parentNode.parentNode.children[0].innerHTML
+
+
       })
   }
   /***** Fermeture de la boîte modale */
