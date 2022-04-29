d146a7d23ad40ff128cd38fb935cf989303d9613
Sara Hébert <e1995310@cmaisonneuve.qc.ca>
2022-04-28 23:15:49 -0400

    ALL FIXED

--- wp-content/themes/TRUE/header.php
+++ wp-content/themes/TRUE/header.php
@@ -1,8 +1,4 @@
-<?php
-/**
- * L'entête « header » de chacune des pages de notre site
- */
-?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -10,14 +6,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <?php wp_head() ?>
-    <style>
-        .home::after{
-            background-color: <?php get_theme_mod( 'background_clippath') ?>;;
-        }
-    </style>
- 
     <?php show_admin_bar(true); ?>
+
+    <style>
+        .home::after {
+            background-color : <?=  get_theme_mod('background_clippath');?>;
+        }
+    </style>    
 </head>
 
 <body <?php body_class("site"); ?> style="background-color:<?=  get_theme_mod('background_body');?>;" >
@@ -60,18 +57,20 @@
                 </svg>
             </div>
             <?php get_search_form(); ?>
-            <!-- form recherche -->
         </div>
     </header>
     <section class="site__barre">
         <input type="checkbox" id="chk-burger">
-        <label for="chk-burger" id="burger">
-            <svg width="32px" height="32px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
+        <label for="chk-burger" id="burger"  class="burger">
+            <!--  svg width="32px" height="32px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
                 color="#ddd">
                 <path fill-rule="evenodd"
                     d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
                     clip-rule="evenodd"></path>
-            </svg>
+            </svg -->
+            <div class="burger__ligne"></div>
+            <div class="burger__ligne"></div>
+            <div class="burger__ligne"></div>
         </label>
         <?php wp_nav_menu(array("menu"=>"principal",
                             "container"=>"nav")); ?>
