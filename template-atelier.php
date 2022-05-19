3069dc4501f03cb45ad7a5d5d2baf16c842adbf1
Sara Hébert <e1995310@cmaisonneuve.qc.ca>
2022-05-19 13:25:09 -0400

    ef-sh- template-atelier hotfix

--- template-atelier.php
+++ template-atelier.php
@@ -14,15 +14,15 @@ get_header();
      <?php if (have_posts()):  the_post(); ?>
      <?php the_post_thumbnail(); ?>
      <h1> <?php the_title() ?></h1>
-     <p class='atelier__desc'><?php the_field('resume'); ?></p>
-     <p class='atelier__anim'><?php the_field('endroit'); ?></p> 
-     <p class='atelier__ddd'><?php the_field('resume'); ?></p>
-     <p class='atelier__ddf'><?php the_field('endroit'); ?></p> 
-     <p class='atelier__semaine'><?php the_field('resume'); ?></p>
-     <p class='atelier__hs'><?php the_field('endroit'); ?></p> 
-     <p class='atelier__hf'><?php the_field('endroit'); ?></p> 
-     <p class='atelier__duree'><?php the_field('resume'); ?></p>
-     <p class='atelier__local'><?php the_field('endroit'); ?></p> 
+     <p class='atelier__desc'><?php the_field('atelier__desc'); ?>hai</p>
+     <p class='atelier__anim'><?php the_field('atelier__anim'); ?></p> 
+     <p class='atelier__ddd'><?php the_field('atelier__ddd'); ?></p>
+     <p class='atelier__ddf'><?php the_field('atelier__ddf'); ?></p> 
+     <p class='atelier__semaine'><?php the_field('atelier__semaine'); ?></p>
+     <p class='atelier__hs'><?php the_field('atelier__hs'); ?></p> 
+     <p class='atelier__hf'><?php the_field('atelier__hf'); ?></p> 
+     <p class='atelier__duree'><?php the_field('atelier__duree'); ?></p>
+     <p class='atelier__local'><?php the_field('atelier__local'); ?></p> 
      <?php 
      $image = get_field('image');
      if( !empty( $image ) ): ?>
